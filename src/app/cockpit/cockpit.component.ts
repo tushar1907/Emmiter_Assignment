130332e618b8bb3dedac3c4bc911c9ef6d1ade0b
Tushar Gupta <gupta.tus@husky.neu.edu>
2018-03-29 10:17:47 -0400

    Emit event

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { Output, ViewChild} from '@angular/core';
+
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  serverElements = [];
+  // newServerName = '';
+  // newServerContent = '';
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent:string}>();
+  @Output('bpCreated') blurprintCreated= new EventEmitter<{serverName: string, serverContent:string}>();
+  @ViewChild('serverContentInput') serverContentInput;
+
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddServer(nameInput: HTMLInputElement) {
+    console.log(nameInput.value);
+    
+    this.serverCreated.emit
+    ({serverName: nameInput.value,
+      serverContent:this.serverContentInput.nativeElement.value
+    });
+
+  }
+
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.blurprintCreated.emit
+    ({serverName: nameInput.value,
+      serverContent:this.serverContentInput.nativeElement.value
+    });
+  }
+}
